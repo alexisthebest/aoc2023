65324c375c32fb236f36f8de6acdadfd263d9fa7
Alex Cotton <alex@Alexs-MacBook-Air-2.local>
2023-12-16 13:21:41 +0000

    wip

--- day12/part2.go
+++ day12/part2.go
@@ -4,14 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
-func doesMatch(line string, groups []int, expectedTotalLength int) (bool, bool) {
-	regex := regexp.MustCompile(`#+`)
-	m := regex.FindAllString(line, -1)
+func counts(line []bool) []int {
+	arr := []int{}
+
+	n := 0
+	for _, c := range line {
+		if c {
+			n++
+		} else {
+			if n > 0 {
+				arr = append(arr, n)
+			}
+			n = 0
+		}
+	}
+	if n > 0 {
+		return append(arr, n)
+	}
+	return arr
+}
+
+func doesMatch(line []bool, groups []int, expectedTotalLength int) (bool, bool) {
+	m := counts(line)
 	lastIndex := len(m) - 1
 
 	if lastIndex >= 0 {
@@ -19,8 +37,7 @@ func doesMatch(line string, groups []int, expectedTotalLength int) (bool, bool) 
 		totalLength := 0
 		valid := false
 
-		for i, str := range m {
-			currentLength := len(str)
+		for i, currentLength := range m {
 			valid = currentLength == groups[j]
 			if i < lastIndex && !valid {
 				return false, false
@@ -36,27 +53,67 @@ func doesMatch(line string, groups []int, expectedTotalLength int) (bool, bool) 
 	return true, false
 }
 
-func process(originalLine []rune, newLine string, groups []int, index int, expected int) int {
+func nextGroupIndex(index int, groups []int, increment int) int {
+	newIndex := index + increment
+
+	if newIndex < 0 {
+		newIndex = len(groups) - 1
+	} else if newIndex > len(groups)-1 {
+		newIndex = 0
+	}
+	return newIndex
+}
+
+func process(index int, originalLine []rune, current rune, groups []int, groupIndex int, seen int, totalSeen int, expectedTotal int) int {
 	total := 0
 
-	partialMatch, fullMatch := doesMatch(newLine, groups, expected)
-
-	if partialMatch {
-		if fullMatch && len(newLine) == len(originalLine) {
-			return total + 1
+	if index < len(originalLine) && totalSeen < expectedTotal {
+		if seen > groups[groupIndex] {
+			return 0
 		}
-		if index < len(originalLine) {
-			current := originalLine[index]
 
-			if current == '#' || current == '.' {
-				total += process(originalLine, string(append([]rune(newLine), current)), groups, index+1, expected)
-			} else {
-				total += process(originalLine, string(append([]rune(newLine), '#')), groups, index+1, expected)
-				total += process(originalLine, string(append([]rune(newLine), '.')), groups, index+1, expected)
+		next := '?'
+		if index < len(originalLine)-1 {
+			next = originalLine[index+1]
+		}
+		switch current {
+		case '#':
+			{
+				total += process(index+1, originalLine, next, groups, groupIndex, seen+1, totalSeen+1, expectedTotal)
+			}
+		case '.':
+			{
+				if seen > 0 {
+					if seen != groups[groupIndex] {
+						return 0
+					}
+					groupIndex = nextGroupIndex(groupIndex, groups, 1)
+				}
+				total += process(index+1, originalLine, next, groups, groupIndex, 0, totalSeen, expectedTotal)
+			}
+		default:
+			{
+				total += process(index, originalLine, '#', groups, groupIndex, seen, totalSeen, expectedTotal)
+				total += process(index, originalLine, '.', groups, groupIndex, seen, totalSeen, expectedTotal)
 			}
 		}
+	} else {
+		if seen == groups[groupIndex] && totalSeen == expectedTotal {
+			return total + 1
+		}
 	}
+
 	return total
+}
+
+func runProcess(channel chan int, line []rune, groups []int) {
+	fmt.Println("Running", string(line), counts, len(line))
+
+	total := process(0, line, line[0], groups, 0, 0, 0, addUp(groups)*5)
+
+	fmt.Println(string(line), ":", total)
+
+	channel <- total
 }
 
 func unfold(line string, join string) string {
@@ -91,20 +148,34 @@ func main() {
 	}
 	scanner := bufio.NewScanner(r)
 
+	channel := make(chan int)
+
 	count := 0
+
+	channelCount := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Split(line, " ")
 
 		spr := []rune(unfold(parts[0], "?"))
+		//spr := []rune(parts[0])
 		counts := parseCounts(parts[1])
 
-		fmt.Println("Running", string(spr), counts, len(spr))
-		n := process(spr, "", counts, 0, addUp(counts)*5)
-		fmt.Println(n)
-		count += n
-		fmt.Println()
+		go runProcess(channel, spr, counts)
+		channelCount += 1
 		//break
 	}
+	n := 0
+	for total := range channel {
+		count += total
+		n++
+		fmt.Println(n, count)
+
+		if n >= channelCount {
+			break
+		}
+	}
+	close(channel)
+
 	fmt.Println(count)
 }
